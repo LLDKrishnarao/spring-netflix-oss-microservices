592370da031faabe36896e58af16a76fa1c41781
LakshmiKrishna <LakshmiKrishna@192.168.1.2>
2017-06-12 09:28:48 +0530

    Add EnableDiscoveryClient and EnableZuulServer to the edge server main application

--- edge-server/src/main/java/com/springnetflixoss/microservices/edgeserver/EdgeServerApplication.java
+++ edge-server/src/main/java/com/springnetflixoss/microservices/edgeserver/EdgeServerApplication.java
@@ -2,8 +2,12 @@ package com.springnetflixoss.microservices.edgeserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulServer
 public class EdgeServerApplication {
 
 	public static void main(String[] args) {
